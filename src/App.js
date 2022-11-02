085a337a779c891be52b309152973089519c6b68
ngoclan49 <ngocdiepdn213@gmail.com>
2022-11-02 11:55:32 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Header from './Components/Header';
+import Banner from './Components/Body/Banner';
+import Item from './Components/Body/Item';
+import Footer from './Components/Footer';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Banner/>
+        <Item/>
+        <Footer/>
+      </div>
+    );
+  }
 }
 
 export default App;
